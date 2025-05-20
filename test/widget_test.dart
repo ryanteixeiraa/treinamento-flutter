fdbaad89674130b99ca14b079f17e6857677dca2
ryanteixeiraa <ryanteixeiraa@gmail.com>
2025-05-20 20:04:03 -0300

    Inclusão de treinamentos estão funcionando, mas não esta salvando o JSON

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:treinamento/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const teste());
+    await tester.pumpWidget(const TreinamentoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
